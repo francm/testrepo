7a0466a3c18b0bdbe9202518739a665450941b94
MFR <franc@fg.cz>
2013-07-19 14:05:26 +0200

    Init

---
+++ src/main/java/com/fg/Test.java
@@ -0,0 +1,18 @@
+package com.fg;
+
+/**
+ * Logger identification (used as Nested Diagnostic Context)
+ *
+ * @author Michal Franc, FG Forrest (c) 2008
+ * @version $Id$
+ *          16.4.2008 10:29:19
+ */
+public interface Test {
+
+    /**
+     * Get identification string
+     * @return
+     */
+    public String getIdentification();
+
+}
